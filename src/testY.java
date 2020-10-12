1b4545977b2693e29ad2ef4934415a97669c1f21
Your Name <you@example.comwangbohpu@163.com>
2020-10-12 11:36:43 +0800

    mulit thread

---
+++ src/testY.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.locks.Condition;
+
+public class testY {
+    String str = new String("good");
+    char[] ch = {'a','b','c','d'};
+    public static void main(String[] args) {
+       int[] arr = null;
+       if(arr == null)
+           System.out.println(111);
+
+}
+    public static void change(String str,char ch[]){
+        str = "test ok";
+        ch[0] = 't';
+    }
+}
+class  Example{
+
+
+}
